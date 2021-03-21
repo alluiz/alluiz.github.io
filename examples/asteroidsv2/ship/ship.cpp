7935b54673842fca4c2be152c6d5249ed4eab079
Luiz Almeida <al.luiz@live.com>
2021-03-20 23:42:46 -0300

    restaurando a nave

--- examples/asteroidsv2/ship/ship.cpp
+++ examples/asteroidsv2/ship/ship.cpp
@@ -19,39 +19,52 @@ void Ship::initializeGL(GLuint program) {
 
   std::array<glm::vec2, 24> positions{
       // Ship body
-      glm::vec2{-02.5f, +12.5f},
-      glm::vec2{-09.5f, -07.5f},
-      glm::vec2{-03.5f, -12.5f},
-      glm::vec2{+03.5f, -12.5f},
-      glm::vec2{+09.5f, -07.5f},
-      glm::vec2{0.0f, -15.5f},
-      glm::vec2{+15.5f, +02.5f},
-      glm::vec2{+02.5f, +12.5f},
+      glm::vec2{-02.5f, +12.5f}, glm::vec2{-15.5f, +02.5f},
+      glm::vec2{-15.5f, -12.5f}, glm::vec2{-09.5f, -07.5f},
+      glm::vec2{-03.5f, -12.5f}, glm::vec2{+03.5f, -12.5f},
+      glm::vec2{+09.5f, -07.5f}, glm::vec2{+15.5f, -12.5f},
+      glm::vec2{+15.5f, +02.5f}, glm::vec2{+02.5f, +12.5f},
 
+      // Cannon left
+      glm::vec2{-12.5f, +10.5f}, glm::vec2{-12.5f, +04.0f},
+      glm::vec2{-09.5f, +04.0f}, glm::vec2{-09.5f, +10.5f},
+
+      // Cannon right
+      glm::vec2{+09.5f, +10.5f}, glm::vec2{+09.5f, +04.0f},
+      glm::vec2{+12.5f, +04.0f}, glm::vec2{+12.5f, +10.5f},
+      
       // Thruster trail (left)
-      glm::vec2{-10.0f, -07.5f},
-      glm::vec2{-07.5f, -18.0f},
-      glm::vec2{-05.0f, -07.5f},
+      glm::vec2{-12.0f, -07.5f}, 
+      glm::vec2{-09.5f, -18.0f}, 
+      glm::vec2{-07.0f, -07.5f},
 
       // Thruster trail (right)
-      glm::vec2{+05.0f, -07.5f},
-      glm::vec2{+07.5f, -18.0f},
-      glm::vec2{+10.0f, -07.5f},
-  };
+      glm::vec2{+07.0f, -07.5f}, 
+      glm::vec2{+09.5f, -18.0f}, 
+      glm::vec2{+12.0f, -07.5f},
+      };
 
-  // Normalize
-  for (auto &position : positions) {
-    position /= glm::vec2{15.5f, 15.5f}; 
+      // Normalize
+  for (auto &position :positions) {
+    position /= glm::vec2{15.5f, 15.5f};
   }
 
-  std::array indices{0, 1, 2, 
-                     0, 2, 3, 
-                     7, 0, 3, 
-                     7, 3, 4, 
-                     2, 3, 5,
+  std::array indices{0, 1, 3,
+                     1, 2, 3,
+                     0, 3, 4,
+                     0, 4, 5,
+                     9, 0, 5,
+                     9, 5, 6,
+                     9, 6, 8,
+                     8, 6, 7,
+                     // Cannons
+                     10, 11, 12,
+                     10, 12, 13,
+                     14, 15, 16,
+                     14, 16, 17,
                      // Thruster trails
-                     8, 9, 10, 
-                     11, 12, 13};
+                     18, 19, 20,
+                     21, 22, 23};
 
   // Generate VBO
   glGenBuffers(1, &m_vbo);
@@ -97,24 +110,6 @@ void Ship::paintGL(const GameData &gameData) {
   glUniform1f(m_scaleLoc, m_scale);
   glUniform1f(m_rotationLoc, m_rotation);
   glUniform2fv(m_translationLoc, 1, &m_translation.x);
-
-  // Restart thruster blink timer every 100 ms
-  if (m_trailBlinkTimer.elapsed() > 100.0 / 1000.0) m_trailBlinkTimer.restart();
-
-  if (gameData.m_input[static_cast<size_t>(Input::Up)]) {
-    // Show thruster trail during 50 ms
-    if (m_trailBlinkTimer.elapsed() < 50.0 / 1000.0) {
-      glEnable(GL_BLEND);
-      glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-      // 50% transparent
-      glUniform4f(m_colorLoc, 1, 1, 1, 0.5f);
-
-      glDrawElements(GL_TRIANGLES, 14 * 3, GL_UNSIGNED_INT, nullptr);
-
-      glDisable(GL_BLEND);
-    }
-  }
 
   glUniform4fv(m_colorLoc, 1, &m_color.r);
   glDrawElements(GL_TRIANGLES, 12 * 3, GL_UNSIGNED_INT, nullptr);
